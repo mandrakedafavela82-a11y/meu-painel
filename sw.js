f441ac01db0aa9189d3cea2a2b96e5aa5702033b
Ryan Barbosa <deploy@capitalcred.com>
2026-07-06 00:14:42 +0000

    v2: backup/importar, follow-up (cobrar hoje), filtro por chip, extrato do mês e meta do mês

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meupainel-v3';
+const CACHE = 'meupainel-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
