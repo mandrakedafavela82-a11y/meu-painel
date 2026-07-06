3d86d63d6b4221c7d1dadac2027b8fc11e360a7e
Ryan Barbosa <deploy@capitalcred.com>
2026-07-06 19:44:04 +0000

    Gerador de recibo em PDF por cliente (dados do emissor no ⚙️, numeração automática)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meupainel-v6';
+const CACHE = 'meupainel-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
