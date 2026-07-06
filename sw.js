4e8c4d8519e495bafd44acea4910ccc141d28252
Ryan Barbosa <deploy@capitalcred.com>
2026-07-06 16:46:38 +0000

    Comissão configurável (%). Pagos/perdidos mantêm a taxa da época

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meupainel-v4';
+const CACHE = 'meupainel-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
